7863aa79af5c1649d69be3345aea27ee64c69c44
tsukudan <kunfujugon0305@gmail.com>
2022-05-23 04:03:40 +0000

    fin cross

--- protos/train.py
+++ protos/train.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from logging import StreamHandler,DEBUG , Formatter , FileHandler, getLogger
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, ParameterGrid
 from sklearn.metrics import log_loss, roc_auc_score
 
 from load_data import load_train_data, load_test_data
@@ -43,31 +43,49 @@ if __name__ == "__main__":
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
 
 
-    list_auc_score = []
-    list_logloss_score = []
-    for train_idx, valid_idx in cv.split(x_train, y_train):
-        trn_x = x_train.iloc[train_idx, :]
-        val_x = x_train.iloc[valid_idx, :]
+    all_params = {"C":[10**i for i in range(-1,2)],
+                  "fit _intercept":[True, False],
+                  "penalty":["l2","l1"],
+                  "random_state":[0]}
+    min_score = 100
+    min_params = None
 
-        trn_y = y_train[train_idx]
-        val_y = y_train[valid_idx]
-
-
-        clf = LogisticRegression(random_state=0)
-        clf.fit(trn_x, trn_y)
-
-        pred = clf.predict_proba(val_x)[:,1]
-        sc_logloss = log_loss(val_y, pred)
-        sc_auc = roc_auc_score(val_y,pred)
-
-        list_logloss_score.append(sc_logloss)
-        list_auc_score.append(sc_auc)
-        
-        
-        logger.debug("    logloss:{}, auc:{}".format(sc_logloss, sc_auc))
+    for params in ParameterGrid(all_params):
+        logger.info("pramas:{}".format(params))       
 
     
-    logger.info("logloss:{}, auc:{}".format(np.mean(list_logloss_score), np.mean(list_auc_score)))        
+        list_auc_score = []
+        list_logloss_score = []
+        for train_idx, valid_idx in cv.split(x_train, y_train):
+            trn_x = x_train.iloc[train_idx, :]
+            val_x = x_train.iloc[valid_idx, :]
+
+            trn_y = y_train[train_idx]
+            val_y = y_train[valid_idx]
+
+
+            clf = LogisticRegression(**params)
+            clf.fit(trn_x, trn_y)
+
+            pred = clf.predict_proba(val_x)[:,1]
+            sc_logloss = log_loss(val_y, pred)
+            sc_auc = - roc_auc_score(val_y,pred)
+
+            list_logloss_score.append(sc_logloss)
+            list_auc_score.append(sc_auc)
+        
+        
+            logger.info("    logloss:{}, auc:{}".format(sc_logloss, sc_auc))
+
+        sc_logloss = np.mean(list_logloss_score)
+        sc_auc = np.mean(list_auc_score)
+        logger.info("logloss:{}, auc:{}".format(sc_logloss, sc_auc))        
+        if min_score > sc_auc:
+            min_score = sc_auc
+            min_params = params
+
+    logger.info("minimum_auc:{}".format(min_params))
+    logger.info("minimum_auc:{}".format(min_score))
     
     clf = LogisticRegression(random_state=0)
     clf.fit(x_train, y_train)
